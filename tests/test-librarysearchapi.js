f148af1f380e4bdd19eff24daed48e510694f25d
Mathieu Decoene <hello@mathieudecoene.be>
2013-09-17 18:37:11 +0100

    Added unit tests to oae-librarysearch

--- tests/test-librarysearchapi.js
+++ tests/test-librarysearchapi.js
@@ -15,47 +15,47 @@
 
 var _ = require('underscore');
 var assert = require('assert');
-var express = require('express');
 
+var ConfigAPI = require('oae-config');
 var ConfigTestUtil = require('oae-config/lib/test/util');
 var RestAPI = require('oae-rest');
 var RestContext = require('oae-rest/lib/model').RestContext;
 var TestsUtil = require('oae-tests');
 
 var ActivityTestsUtil = require('oae-activity/lib/test/util');
-var LibrarySearchConfig = require('oae-config').config('oae-librarysearch');
+var LibrarySearchAPIConfig = require('oae-config').config('oae-librarysearch');
+var LibrarySearchAPIConstants = require('../lib/constants').LibrarySearchAPIConstants;
+
+// Require the REST client for librarysearch. It will be made available on `RestAPI.LibrarySearch`
+RestAPI.LibrarySearch = require('oae-librarysearch/lib/client');
 
 describe('LibrarySearchAPI', function() {
 
     // Rest context that can be used every time we need to make a request as a cam tenant admin
     var camAdminRestContext = null;
 
+    // The original correct endpoints for the external api's
+    var originalAquabrowserEndpoint = null;
+    var originalSummonEndpoint = null;
+
     /**
-    * Initializes the admin REST contexts
-    */
+     * Initializes the admin REST contexts
+     */
     before(function(callback) {
 
         // Fill up the cam admin rest context
         camAdminRestContext = TestsUtil.createTenantAdminRestContext(global.oaeTests.tenants.cam.host);
 
-        // Create a new express application to mock a LibrarySearch
-        var app = express();
-        app.use(express.bodyParser({}));
-
-        app.post('/', function(req, res) {
-            onRequest(req);
-            res.send(200);
-        });
-
-        // Listen to a specific port
-        var server = app.listen(3000);
+        // Fill up the original endpoints for the external api's
+        originalAquabrowserEndpoint = LibrarySearchAPIConfig.getValue(global.oaeTests.tenants.cam.alias, 'aquabrowser', 'endpoint');
+        originalSummonEndpoint = LibrarySearchAPIConfig.getValue(global.oaeTests.tenants.cam.alias, 'summon', 'endpoint');
 
         callback();
     });
 
     /**
-    * Disables LibrarySearch, Aquabrowser and Summon for the tenant after each test
-    */
+     * Disables LibrarySearch, Aquabrowser and Summon for the tenant after each test
+     */
     afterEach(function(callback){
 
         // Disable LibrarySearch for tenant
@@ -69,12 +69,22 @@ describe('LibrarySearchAPI', function() {
                 // Disable Summon for tenant
                 ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/summon/enabled', false, function(err) {
                     assert.ok(!err);
-                    callback();
+
+                    // Restore the original endpoint for Aquabrowser
+                    ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/aquabrowser/endpoint', originalAquabrowserEndpoint, function(err) {
+                        assert.ok(!err);
+                    
+                        // Restore the original endpoint for Summon
+                        ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/summon/endpoint', originalSummonEndpoint, function(err) {
+                            assert.ok(!err);
+                            callback();
+                        });
+                    });
                 });
             });
         });
     });
-
+    
     /**
      * Test that verifies that no requests can be send to Summon or Aquabrowser when LibrarySearch is disabled for tenant
      */
@@ -88,14 +98,35 @@ describe('LibrarySearchAPI', function() {
             TestsUtil.generateTestUsers(camAdminRestContext, 1, function(err, users) {
                 assert.ok(!err);
 
-                _.each(users, function(user, id) {
-                    var userCtx = user.restContext;
+                // Perform a search request
+                var testUser = _.values(users)[0];
+                RestAPI.LibrarySearch.search(testUser.restContext, 'darwin', function(err, data) {
+                    assert.ok(err);
+                    assert.deepEqual(err, LibrarySearchAPIConstants.errors.LIBRARYSEARCH_DISABLED);
+                    callback();
+                });
+            });
+        });
+    });
 
-                    onRequest = function(req) {
+    /**
+     * Test that verifies that a valid query has been entered
+     */
+    it('verify query in params is not empty', function(callback) {
 
-                    };
+        // First enable LibrarySearch since the default value is false
+        ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/librarysearch/enabled', true, function(err) {
+            assert.ok(!err);
 
-                    // Perform a search to the LibrarySearch API
+            // Generate a test user
+            TestsUtil.generateTestUsers(camAdminRestContext, 1, function(err, users) {
+                assert.ok(!err);
+
+                // Perform a search request with an empty query
+                var testUser = _.values(users)[0];
+                RestAPI.LibrarySearch.search(testUser.restContext, '', function(err, data) {
+                    assert.ok(err);
+                    assert.deepEqual(err, LibrarySearchAPIConstants.errors.INVALID_QUERY);
                     callback();
                 });
             });
@@ -115,19 +146,16 @@ describe('LibrarySearchAPI', function() {
             TestsUtil.generateTestUsers(camAdminRestContext, 1, function(err, users) {
                 assert.ok(!err);
 
-                _.each(users, function(user, id) {
-                    var userCtx = user.restContext;
-
-                    onRequest = function(req) {
-
-                    };
-
-                    // Perform a search to the LibrarySearch API
+                // Perform a search request
+                var testUser = _.values(users)[0];
+                RestAPI.LibrarySearch.search(testUser.restContext, 'darwin', function(err, data) {
+                    assert.ok(err);
+                    assert.deepEqual(err, LibrarySearchAPIConstants.errors.EXTERNAL_APIS_DISABLED);
                     callback();
                 });
             });
         });
-    });    
+    });
 
     /**
      * Test that verifies the connection between OAE and Aquabrowser API
@@ -139,21 +167,17 @@ describe('LibrarySearchAPI', function() {
             assert.ok(!err);
 
             // Enable Aquabrowser for the tenant
-            ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/aquabrowser/enabled', true, function(err) {
-                assert.ok(!err);
+            destroyExternalAPI('aquabrowser', function() {
 
                 // Generate a test user
                 TestsUtil.generateTestUsers(camAdminRestContext, 1, function(err, users) {
                     assert.ok(!err);
 
-                    _.each(users, function(user, id) {
-                        var userCtx = user.restContext;
-
-                        onRequest = function(req) {
-
-                        };
-
-                        // Perform a search to the LibrarySearch API
+                    // Perform a search request
+                    var testUser = _.values(users)[0];
+                    RestAPI.LibrarySearch.search(testUser.restContext, 'darwin', function(err, data) {
+                        assert.ok(!err);
+                        assert.ok(data.aquabrowser.error);
                         callback();
                     });
                 });
@@ -170,25 +194,121 @@ describe('LibrarySearchAPI', function() {
         ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/librarysearch/enabled', true, function(err) {
             assert.ok(!err);
 
-            // Enable Summon for the tenant
-            ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/summon/enabled', true, function(err) {
-                assert.ok(!err);
+            // Enable Aquabrowser for the tenant
+            destroyExternalAPI('summon', function() {
 
                 // Generate a test user
                 TestsUtil.generateTestUsers(camAdminRestContext, 1, function(err, users) {
+                    assert.ok(!err);
 
-                    _.each(users, function(user, id) {
-                        var userCtx = user.restContext;
-
-                        onRequest = function(req) {
-
-                        };
-
-                        // Perform a search to the LibrarySearch API
+                    // Perform a search request
+                    var testUser = _.values(users)[0];
+                    RestAPI.LibrarySearch.search(testUser.restContext, 'darwin', function(err, data) {
+                        assert.ok(!err);
+                        assert.ok(data.summon.error);
                         callback();
                     });
                 });
             });
         });
     });
+
+    /**
+     * Test that verifies the result when an error is thrown for one of the external api's
+     */
+    it('verify result when Aquabrowser is valid and Summon is broken', function(callback) {
+
+        // Store the tenantAlias
+        var tenantAlias = global.oaeTests.tenants.cam.alias;
+
+        // First enable LibrarySearch since the default value is false
+        ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/librarysearch/enabled', true, function(err) {
+            assert.ok(!err);
+
+            // Destroy Summon for tenant
+            destroyExternalAPI('summon', function() {
+
+                // Enable Aquabrowser for tenant
+                ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/aquabrowser/enabled', true, function(err) {
+                    assert.ok(!err);
+
+                    // Generate a test user
+                    TestsUtil.generateTestUsers(camAdminRestContext, 1, function(err, users) {
+                        assert.ok(!err);
+
+                        // Perform a search request
+                        var testUser = _.values(users)[0];
+                        RestAPI.LibrarySearch.search(testUser.restContext, 'darwin', function(err, data) {
+                            assert.ok(!err);
+                            assert.ok(!data.aquabrowser.error);
+                            assert.ok(data.aquabrowser.total);
+                            assert.ok(data.aquabrowser.results);
+                            assert.ok(data.summon.error);
+                            callback();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+
+    /**
+     * Test that verifies the result when an error is thrown for one of the external api's
+     */
+    it('verify result when Summon is valid and Aquabrowser is broken', function(callback) {
+
+        // Store the tenantAlias
+        var tenantAlias = global.oaeTests.tenants.cam.alias;
+
+        // First enable LibrarySearch since the default value is false
+        ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/librarysearch/enabled', true, function(err) {
+            assert.ok(!err);
+
+            // Destroy Summon for tenant
+            destroyExternalAPI('aquabrowser', function() {
+
+                // Enable Aquabrowser for tenant
+                ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/summon/enabled', true, function(err) {
+                    assert.ok(!err);
+
+                    // Generate a test user
+                    TestsUtil.generateTestUsers(camAdminRestContext, 1, function(err, users) {
+                        assert.ok(!err);
+
+                        // Perform a search request
+                        var testUser = _.values(users)[0];
+                        RestAPI.LibrarySearch.search(testUser.restContext, 'darwin', function(err, data) {
+                            assert.ok(!err);
+                            assert.ok(!data.summon.error);
+                            assert.ok(data.summon.total);
+                            assert.ok(data.summon.results);
+                            assert.ok(data.aquabrowser.error);
+                            callback();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    /**
+     * Whilst enabling an external API for LibrarySearch, use false information for its authentication to generate an error
+     * 
+     * @param  {String}          api                The name of the external API
+     * @param  {Function(data)}  callback           Standard callback function
+     */
+    var destroyExternalAPI = function(api, callback) {
+
+        // Enable Aquabrowser for the tenant
+        ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/' + api + '/enabled', true, function(err) {
+            assert.ok(!err);
+
+            // Change the Aquabrowser endpoint to a fake one so it will throw an error
+            ConfigTestUtil.updateConfigAndWait(camAdminRestContext, null, 'oae-librarysearch/' + api + '/endpoint', 'fault', function(err) {
+                assert.ok(!err);
+                callback();
+            });
+        });
+    };
 });
